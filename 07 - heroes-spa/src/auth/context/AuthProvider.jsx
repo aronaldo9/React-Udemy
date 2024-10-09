c154321fcabd822c78cae0da64b6c04b6902006e
aronaldo9 <aaronpesqueirafarina@gmail.com>
2024-10-10 00:38:36 +0200

    finalizado router heroes-app

--- 07 - heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07 - heroes-spa/src/auth/context/AuthProvider.jsx
@@ -9,16 +9,38 @@ const initialState = {
   logged: false,
 };
 
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return {
+    logged: !!user,
+    user: user,
+  };
+};
+
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
   const login = (name = "") => {
+    const user = {
+      id: "ABC",
+      name,
+    };
+
     const action = {
       type: types.login,
-      payload: {
-        id: "ABC",
-        name: name,
-      },
+      payload: user,
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    dispatch(action);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    const action = {
+      type: types.logout,
     };
     dispatch(action);
   };
@@ -27,7 +49,10 @@ export const AuthProvider = ({ children }) => {
     <AuthContext.Provider
       value={{
         ...authState,
-        login: login,
+
+        // Methods
+        login,
+        logout,
       }}
     >
       {children}
